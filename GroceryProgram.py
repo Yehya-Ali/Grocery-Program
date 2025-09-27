19c4ba2217f59ea3270a99b1d2f679b9b91ab16a
Yehya-Ali <yehya.z.ali@gmail.com>
2025-09-27 14:42:41 +0200

    V2: Adding loop condition to calculate price

--- GroceryProgram.py
+++ GroceryProgram.py
@@ -4,16 +4,27 @@ fruit = ""
 
 cart = []
 
+price = 0
+
 while fruit != "done":
 
     fruit = input("What do you want to buy?")
 
     if fruit in groceries:
-
         cart.append(fruit)
         
     else:
-
         print("Sorry, we don't have that item.")
 
+for i in cart:
+    if i in groceries:
+        price += groceries[i]
+
 print(f"You bought: {cart}")
+print(f"Total: ${price}")
+
+if price > 10:
+    print("You spent a lot!")
+
+else:
+    print("You spent a little!")
